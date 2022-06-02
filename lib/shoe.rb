cc3910b626585adbd6a1bd4b8ef6d09d34c5bdd1
andyjohn23 <andrewnick906@gmail.com>
2022-06-02 14:50:27 +0300

    completed the shoe file tests

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+end
+
+shoe = Shoe.new("Alexander Mcqueen")
+shoe.brand
+shoe.cobble
+
+shoe_condition = Shoe.new("new")
+shoe_condition.condition
