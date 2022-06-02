d89c7256ef12988644c95fba5e9927a406401987
andyjohn23 <andrewnick906@gmail.com>
2022-06-02 13:45:27 +0300

    completed the books tests that is successful

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    
+    def initialize(title)
+        @title = title  
+    end
 
+    def author=(author)
+        @author = author
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def genre=(genre)
+        @genre = genre 
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+book= Book.new("You can win")
+book.title
+book.turn_page
+
+book_author = Book.new("Shiv Khera")
+book_author.author
+
+book_pages = Book.new(299)
+book_pages.page_count
+
+book_genre = Book.new("Motivational books")
+book_genre.genre
 
